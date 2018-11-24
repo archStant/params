ab2e218916df8da38cca77e4ba3784e0859f5993
Roald Simonsen <wxd401@alumni.ku.dk>
2018-11-24 22:46:56 +0100

    Better error messages and helpstring

--- parapy/__init__.py
+++ parapy/__init__.py
@@ -1,9 +1,32 @@
 import sys
 
 name = "params"
+helpString = '''\033[0;1mParapy help\033[0m
+Use this library by defining a list of tuples of your parameters,
+and then calling parapy.params(YOU_PARAMETERS). See example below:
+-----
+parameters = [('output', '-o', str),    # A string parameter with prefix '-o'
+              ('amount', '-n', int),    # An integer parameter with prefix '-n'
+              ('verbose', '-v', bool),  # A boolean parameter with name '-v'
+              ('eggs', '-e', float),    # A float parameter with prefix '-e'
+              ('input', 0, str),        # A string parameter position 0
+              ('spam', 1, int),         # An integer parameter position 1
+              ('message', '-m', str)]   # A string parameter with prefix '-m'
+
+
+p = (parapy.params(parameters))
+-----
+'''
+
+
+def help():
+    print(helpString)
 
 
 def params(parameters):
+    if not ((type(parameters) == list) and (len(parameters) > 0)):
+        print('\033[91;1mError\033[0m: parameter configuration not valid.\nCall parapy.help() for en example.')
+        return None
     argv = sys.argv
     posParams = list(filter(lambda x: type(x[1]) == int, parameters))
     namedParams = list(filter(lambda x: x not in posParams, parameters))
@@ -18,7 +41,7 @@ def params(parameters):
             try:
                 retval = t(argv[a])
             except ValueError:
-                print('\033[91mError:\033[0m parameter %s is not formatted properly. Is (%s) but expected (%s)' %
+                print('\033[91mError:\033[0m parameter %s is not formatted properly. Is (%s) but expected (%s).' %
                       (x, str(type(argv[a])), str(t)))
                 retval = None
             return retval
@@ -33,7 +56,7 @@ def params(parameters):
         elif (i[2] in allowedTypes):
             i_val = findElse(i[1], i[2])
         else:
-            print('\033[91;1mError:\033[0m parameter %s has an invalid type: (%s)' %
+            print('\033[91;1mError:\033[0m parameter %s has an invalid type: (%s)\nCall parapy.help() for en example configuration.' %
                   (i[0], str(i[2])))
             i_val = None
         if (i[1] in argv):
@@ -46,7 +69,7 @@ def params(parameters):
             try:
                 retval[i[0]] = i[2](argv[i[1]])
             except ValueError:
-                print('\033[91mError:\033[0m parameter %s is not formatted properly. Is (%s) but expected (%s)' %
+                print('\033[91mError:\033[0m parameter %s is not formatted properly. Is (%s) but expected (%s).' %
                       (i[0], str(type(argv[i[1]])), str(i[2])))
                 retval[i[0]] = None
         else:
